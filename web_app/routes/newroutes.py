52c89721d7417b310a9c4f55245f32c67a4c56f1
mad378 <59628155+mad378@users.noreply.github.com>
2020-02-18 19:19:31 -0500

    Completed sendgrid api implementation

--- web_app/routes/newroutes.py
+++ web_app/routes/newroutes.py
@@ -1,7 +1,16 @@
 
 from flask import Blueprint, request, render_template, jsonify, flash, redirect #, url_for
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 new_routes = Blueprint("new_routes", __name__)
+
+load_dotenv()
+
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+MY_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
 
 @new_routes.route('/myform')
 def myform():
@@ -13,7 +22,50 @@ def sendemail():
     print("SENDING AN EMAIL...")
     print("FORM DATA:", dict(request.form))
     #return jsonify(request.form)
-
+    email_address = request.form["email_address"]
     #product_name = request.form["product_name"]
-    flash(f"Sent email to Sebastian esquire", "success") # use the "success" category to correspond with twitter bootstrap alert colors
+
+
+    
+    client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
+    print("CLIENT:", type(client))
+
+    subject = "Testing 1,2,3..."
+
+    html_content = "Hello World"
+    print("HTML:", html_content)
+
+    message = Mail(from_email=MY_ADDRESS, to_emails=email_address, subject=subject, html_content=html_content)
+
+    try:
+        response = client.send(message)
+
+        print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+        print(response.status_code) #> 202 indicates SUCCESS
+        print(response.body)
+        print(response.headers)
+
+    except Exception as e:
+        print("OOPS", e.message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    flash(f"Sent email to {email_address}", "success") # use the "success" category to correspond with twitter bootstrap alert colors
     return redirect("/myform")
